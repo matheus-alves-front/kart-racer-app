e263f633730fdbd9febc389dbd92c413fd998191
Matheus Alves <matheus.alves.front.m2@gmail.com>
2024-04-21 20:55:20 -0300

    init project

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function HomePage() {
+  return (
+      <View>
+        <Text>HomePage</Text>
+      </View>
+  )
+}
